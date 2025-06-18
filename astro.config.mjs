6ca1bf116d7dc41f9f1b00f745493053045bd298
williamhelmrath <wph2tu@virginia.edu>
2025-06-18 01:06:57 -0400

    add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     open: true,
   },
-
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: cloudflare(),
 });
