119ae9c2a6f654e845ad5db8f5ee8203a908be3e
williamhelmrath <wph2tu@virginia.edu>
2025-06-18 00:57:48 -0400

    astro commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    open: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
